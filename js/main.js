55ec358dd41c03778f3fd952d43fa9dac44f4a7d
Dinesh Jayasiri <ywdinesh95@gmail.com>
2023-12-04 13:18:24 +0530

    add terms_of_service

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
+// collapsible components
 const collapsible = document.querySelectorAll(".collapsible");
 
 collapsible.forEach((item)=> item.addEventListener('click', function(){
     this.classList.toggle('collapsible--expanded');
 }))
+
+// close menu
+function closeMenu() {
+  document.getElementById("menu-toggle").checked = false;
+}
+
+// slider 
+
+      var ssSplide = new Splide("#ss", {
+        type: "loop",
+        perPage: 5,
+        focus: "center",
+        autoplay: true,
+        trimSpace: false,
+        breakpoints: {
+          1000: {
+            perPage: 3,
+          },
+          618: {
+            perPage: 1,
+          },
+        },
+      });
+      ssSplide.mount();
+
+      var featureSplide = new Splide("#feature__slide", {
+        type: "loop",
+        perPage: 1,
+        autoplay: true,
+      });
+
+      featureSplide.mount();
