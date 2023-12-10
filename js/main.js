5707a65891a91ed292517c35702f9f0a1d786ee4
Dinesh Jayasiri <ywdinesh95@gmail.com>
2023-12-10 12:00:16 +0530

    update qr

--- js/main.js
+++ js/main.js
@@ -1,38 +1,57 @@
 // collapsible components
 const collapsible = document.querySelectorAll(".collapsible");
 
-collapsible.forEach((item)=> item.addEventListener('click', function(){
-    this.classList.toggle('collapsible--expanded');
-}))
+collapsible.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
 
 // close menu
 function closeMenu() {
   document.getElementById("menu-toggle").checked = false;
 }
 
-// slider 
+// slider
 
-      var ssSplide = new Splide("#ss", {
-        type: "loop",
-        perPage: 5,
-        focus: "center",
-        autoplay: true,
-        trimSpace: false,
-        breakpoints: {
-          1000: {
-            perPage: 3,
-          },
-          618: {
-            perPage: 1,
-          },
-        },
-      });
-      ssSplide.mount();
+var ssSplide = new Splide("#ss", {
+  type: "loop",
+  perPage: 5,
+  focus: "center",
+  autoplay: true,
+  trimSpace: false,
+  interval: 15000,
+  breakpoints: {
+    1000: {
+      perPage: 3,
+    },
+    618: {
+      perPage: 1,
+    },
+  },
+});
+ssSplide.mount();
 
-      var featureSplide = new Splide("#feature__slide", {
-        type: "loop",
-        perPage: 1,
-        autoplay: true,
-      });
+var featureSplide = new Splide("#feature__slide", {
+  type: "loop",
+  perPage: 1,
+  autoplay: true,
+  interval: 15000,
+  breakpoints: {
+    1024: {
+      pagination: false,
+    },
+  },
+});
 
-      featureSplide.mount();
+featureSplide.mount();
+
+// copy rights year
+window.onload = function () {
+  var currentYear = new Date().getFullYear();
+  var elements = document.getElementsByClassName("copyrightYear");
+
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].textContent = currentYear;
+  }
+};
